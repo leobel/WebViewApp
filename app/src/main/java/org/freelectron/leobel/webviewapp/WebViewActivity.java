1d6e4abba0344ccc34f5b6ae1be55380dde579f9
Leobel Izquierdo <leobelizquierdo@gmail.com>
2017-03-20 15:04:55 -0400

    handle invitation with unavailable app

--- app/src/main/java/org/freelectron/leobel/webviewapp/WebViewActivity.java
+++ app/src/main/java/org/freelectron/leobel/webviewapp/WebViewActivity.java
@@ -1,21 +1,93 @@
 package org.freelectron.leobel.webviewapp;
 
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebViewActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private Pattern pattern = Pattern.compile("^whatsapp://send\\?text=(.*)|^sms:\\?body=(.*)|^mailto:\\?&body=(.*)");
+    private AlertDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
-        WebView webView = (WebView) findViewById(R.id.web_view);
+        mDialog = new AlertDialog.Builder(this)
+                .setTitle(R.string.invitation_dialog_title)
+                .setPositiveButton(R.string.invitation_dialog_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        mDialog.dismiss();
+                    }
+                })
+                .create();
 
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.getSettings().setJavaScriptEnabled(true);
+        mWebView = (WebView) findViewById(R.id.web_view);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return handleUrl(url);
+            }
 
-        webView.loadUrl(getString(R.string.site_url));
+            @TargetApi(Build.VERSION_CODES.N)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return handleUrl(request.getUrl().toString());
+            }
+        });
+
+        mWebView.loadUrl(getString(R.string.site_url));
+    }
+
+    private boolean handleUrl(String url) {
+        Matcher matcher = pattern.matcher(url);
+        if(matcher.matches()){
+            if(matcher.group(1) != null) { // whastapp invitation
+                mDialog.setMessage(getString(R.string.whatsapp_not_available));
+            }
+            else if(matcher.group(2) != null){ // sms invitation
+                mDialog.setMessage(getString(R.string.sms_not_available));
+            }
+            else{ // email invitation
+                mDialog.setMessage(getString(R.string.email_not_available));
+            }
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            if(intent.resolveActivity(getPackageManager()) != null){
+                startActivity(intent);
+            }
+            else{
+                mDialog.show();
+            }
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()){
+            mWebView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
